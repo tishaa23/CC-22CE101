2b6a46aeaab494d7ce8a7587cc669230495845af
tishaa23 <pateltisha2304@gmail.com>
2024-12-24 09:33:09 +0530

    made dynamic

--- pract2.cpp
+++ pract2.cpp
@@ -1,53 +1,96 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
-main()
-{
-    int no_of_symbol=2;
-    char symbol[2]={'a','b'};
-    int states=4;
-    int initial=0; //1st state i.e 0
-    int no_accept=1;
-    int accept_state=1; //2nd state means index=0+1=1
-  /*  int arr[2][4];
-    arr[0][0]=1;
-    arr[1][0]=2;
-    arr[0][1]=0;
-    arr[1][1]=3;
-    arr[0][2]=3;
-    arr[1][2]=0;
-    arr[0][3]=2;
-    arr[1][3]=1;*/
-    int arr[2][4] = {
-        {1, 0, 3, 2},  // Transitions for 'a'
-        {2, 3, 0, 1}   // Transitions for 'b'
-    };
-    string str="abbabab";
-    int current_state=initial;
-    for(int i=0;i<str.length();i++)
-    {
-        char current_symbol=str[i];
-        int symbol_index;
-        if(current_symbol=='a')
-        {
-           symbol_index=0;
-        }
-        else
-        {
-           symbol_index=1;
-        }
-        current_state=arr[symbol_index][current_state];
-        cout << "After processing symbol '" << current_symbol << "', current state is: " << current_state << endl;
+int main() {
 
+    int no_of_symbol;
+    cout << "Enter the number of symbols: ";
+    cin >> no_of_symbol;
+
+    vector<char> symbols(no_of_symbol);
+    cout << "Enter the symbols: ";
+    for (int i = 0; i < no_of_symbol; i++) {
+        cin >> symbols[i];
     }
 
-    if(current_state == accept_state) {
+
+    int states;
+    cout << "Enter the number of states: ";
+    cin >> states;
+
+
+    vector<vector<int>> transition_table(no_of_symbol, vector<int>(states));
+    cout << "Enter the transition table:" << endl;
+    for (int i = 0; i < no_of_symbol; i++) {
+        for (int j = 0; j < states; j++) {
+            cout << "Transition for symbol '" << symbols[i] << "' from state " << j << ": ";
+            cin >> transition_table[i][j];
+        }
+    }
+
+
+    int initial_state;
+    cout << "Enter the initial state: ";
+    cin >> initial_state;
+
+
+    int no_accept;
+    cout << "Enter the number of accepting states: ";
+    cin >> no_accept;
+
+    vector<int> accept_states(no_accept);
+    cout << "Enter the accepting states: ";
+    for (int i = 0; i < no_accept; i++) {
+        cin >> accept_states[i];
+    }
+
+
+    string str;
+    cout << "Enter the string to process: ";
+    cin >> str;
+
+
+    int current_state = initial_state;
+    for (int i = 0; i < str.length(); i++) {
+        char current_symbol = str[i];
+        int symbol_index = -1;
+
+
+        for (int j = 0; j < no_of_symbol; j++) {
+            if (current_symbol == symbols[j]) {
+                symbol_index = j;
+                break;
+            }
+        }
+
+
+        if (symbol_index == -1) {
+            cout << "Invalid symbol '" << current_symbol << "' in the string!" << endl;
+            return 1;
+        }
+
+
+        current_state = transition_table[symbol_index][current_state];
+        cout << "After processing symbol '" << current_symbol << "', current state is: " << current_state << endl;
+    }
+
+
+    bool is_accepted = false;
+    for (int i = 0; i < no_accept; i++) {
+        if (current_state == accept_states[i]) {
+            is_accepted = true;
+            break;
+        }
+    }
+
+    if (is_accepted) {
         cout << "The input string is accepted." << endl;
     } else {
         cout << "The input string is not accepted." << endl;
     }
 
-
-
-
+    return 0;
 }
+
